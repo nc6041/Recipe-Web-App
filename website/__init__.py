7189225802075e7308ea8ca0da381fb1877231ea
Natalie Cheng <nataliec6041@gmail.com>
2022-01-21 20:18:44 -0600

    auth urls added

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '84jf-02+kdfm3s?2fs6n'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
